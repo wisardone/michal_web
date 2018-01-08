27fcd6b64a63b569156a14762c528391e8df3e19
wisardone <wisardone@gmail.com>
2018-01-08 16:52:20 +0000

    fuctions

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,18 @@
 var pContainerHeight = document.querySelector('.dron-box').offsetHeight;
 
-window.addEventListener('sroll'), function (){
-    var wScroll = this.pageYOffset;
-    if(wScroll <= pContainerHeight){
-        
-        document.querySelector('.logo').style.
-        transform = 'translate(0px, '+ wScroll / 2 + '%)';
-        
-        document.querySelector('.birds').style.
-        transform = 'translate(0px, '+ wScroll /4 +'%)';
+window.addEventListener('scroll', function(){
+
+  var wScroll = this.pageYOffset;
+
+  if (wScroll <= pContainerHeight) {
+
+    document.querySelector('.logo').style.
+      transform = 'translate(0px, '+ wScroll /2 +'%)';
+
+    document.querySelector('.birds').style.
+      transform = 'translate(0px, '+ wScroll /4 +'%)';
 
     document.querySelector('.drone').style.
       transform = 'translate(0px, -'+ wScroll /40 +'%)';
 
-    }
-}
+  }
